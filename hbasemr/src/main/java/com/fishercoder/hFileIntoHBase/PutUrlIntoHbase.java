0149f9d9f2ef18e2d7335e4601728bedda0bd1e0
stevesun <stevesun@coupang.com>
2017-06-01 18:03:23 -0700

    load HFile into HBase

---
+++ hbasemr/src/main/java/com/fishercoder/hFileIntoHBase/PutUrlIntoHbase.java
@@ -0,0 +1,73 @@
+package com.fishercoder.hFileIntoHBase;
+
+/**
+ * Created by stevesun on 6/1/17.
+ */
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.HFileOutputFormat2;
+import org.apache.hadoop.hbase.mapreduce.PutSortReducer;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+
+public class PutUrlIntoHbase extends Configured implements Tool {
+    /**This program isn't working yet, it's giving me this error:
+     * Exception in thread "main" java.io.IOException: Mkdirs failed to create /user/stevesun/hbase-staging (exists=false, cwd=file:/Users/stevesun/personal_dev/HBaseMapReduceExample/hbasemr)
+     at org.apache.hadoop.fs.ChecksumFileSystem.create(ChecksumFileSystem.java:440)
+     at org.apache.hadoop.fs.ChecksumFileSystem.create(ChecksumFileSystem.java:426)
+     at org.apache.hadoop.fs.FileSystem.create(FileSystem.java:906)
+     at org.apache.hadoop.io.SequenceFile$Writer.<init>(SequenceFile.java:1071)
+     at org.apache.hadoop.io.SequenceFile$RecordCompressWriter.<init>(SequenceFile.java:1371)
+     at org.apache.hadoop.io.SequenceFile.createWriter(SequenceFile.java:272)
+     at org.apache.hadoop.io.SequenceFile.createWriter(SequenceFile.java:294)
+     at org.apache.hadoop.hbase.mapreduce.HFileOutputFormat2.writePartitions(HFileOutputFormat2.java:335)
+     at org.apache.hadoop.hbase.mapreduce.HFileOutputFormat2.configurePartitioner(HFileOutputFormat2.java:596)
+     at org.apache.hadoop.hbase.mapreduce.HFileOutputFormat2.configureIncrementalLoad(HFileOutputFormat2.java:440)
+     at org.apache.hadoop.hbase.mapreduce.HFileOutputFormat2.configureIncrementalLoad(HFileOutputFormat2.java:405)
+     at org.apache.hadoop.hbase.mapreduce.HFileOutputFormat2.configureIncrementalLoad(HFileOutputFormat2.java:367)
+     at com.fishercoder.hFileIntoHBase.PutUrlIntoHbase.run(PutUrlIntoHbase.java:41)
+     at org.apache.hadoop.util.ToolRunner.run(ToolRunner.java:70)
+     at com.fishercoder.hFileIntoHBase.PutUrlIntoHbase.main(PutUrlIntoHbase.java:51)
+
+     I asked on Stackoverflow: https://stackoverflow.com/questions/44318693/java-io-ioexception-mkdirs-failed-to-create-when-running-mapreduce-job
+     no answers yet.*/
+
+    private static final String MAPRED_JOB_NAME = "mapred.job.name";
+    private static final String HBASE_TABLE = "mapred.job.name";
+
+    public int run(String[] arg0) throws Exception {
+        System.out.println("In run.........");
+        Configuration conf = new Configuration();
+        conf.set(MAPRED_JOB_NAME, "steve_test");
+        conf.set(HBASE_TABLE, "steve1");
+        Job job = new Job(conf, conf.get(MAPRED_JOB_NAME));
+        String output_table = conf.get(HBASE_TABLE);
+
+        job.setJarByClass(PutUrlIntoHbase.class);
+        job.setMapperClass(PutUrlIntoHbaseMapper.class);
+        job.setReducerClass(PutSortReducer.class);
+
+        job.setMapOutputKeyClass(ImmutableBytesWritable.class);
+        job.setMapOutputValueClass(Put.class);
+
+        HTable table = new HTable(conf, output_table);
+        job.setOutputFormatClass(HFileOutputFormat2.class);
+        HFileOutputFormat2.configureIncrementalLoad(job, table);
+
+        if (job.waitForCompletion(true) && job.isSuccessful()) {
+            return 0;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = HBaseConfiguration.create();
+        int res = ToolRunner.run(conf, new PutUrlIntoHbase(), args);
+        System.exit(res);
+    }
+}
---
+++ hbasemr/src/main/java/com/fishercoder/hFileIntoHBase/PutUrlIntoHbaseMapper.java
@@ -0,0 +1,80 @@
+package com.fishercoder.hFileIntoHBase;
+
+/**
+ * Created by stevesun on 6/1/17.
+ */
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class PutUrlIntoHbaseMapper
+        extends Mapper<LongWritable, Text, ImmutableBytesWritable, Put> {
+
+    public static String toHexString(byte[] bytes) {
+        StringBuilder hexString = new StringBuilder();
+
+        for (int i = 0; i < bytes.length; i++) {
+            String hex = Integer.toHexString(0xFF & bytes[i]);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+
+        return hexString.toString();
+    }
+
+    private String md5(String s) throws NoSuchAlgorithmException {
+        byte b[] = MessageDigest.getInstance("MD5").digest(s.getBytes());
+        return toHexString(b);
+    }
+
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        if (value == null) {
+            context.getCounter("user_mapper", "TOTAL").increment(1L);
+            return;
+        }
+
+        try {
+            String line = value.toString();
+            String[] parts = line.split("\t");
+            if (parts.length != 8) {
+                context.getCounter("user_mapper", "INVALID").increment(1L);
+                return;
+            }
+
+            byte[] sitemap_id           = Bytes.toBytes(parts[0].trim()); // top level sitemap id
+            byte[] top_level_sitemap    = Bytes.toBytes(parts[1].trim());
+            byte[] second_level_sitemap = Bytes.toBytes(parts[2].trim());
+            byte[] url                  = Bytes.toBytes(parts[3].trim());
+            byte[] parse_time           = Bytes.toBytes(parts[4].trim());
+            byte[] sitemap_type         = Bytes.toBytes(parts[7].trim());
+            byte[] k                    = Bytes.toBytes(md5(parts[3].trim()+"#_#!"+parts[7]) ); // key for hbase
+            long r_timestamp = 5000000000000L - System.currentTimeMillis();
+
+            Put put = new Put(k, r_timestamp); // key : url_sitemapType
+            put.addImmutable(Bytes.toBytes("i"), Bytes.toBytes("url"), url);
+            put.addImmutable(Bytes.toBytes("i"), Bytes.toBytes("smap_id"), sitemap_id);
+            put.addImmutable(Bytes.toBytes("i"), Bytes.toBytes("smap_type"), sitemap_type);
+            put.addImmutable(Bytes.toBytes("i"), Bytes.toBytes("top_smap"), top_level_sitemap);
+            put.addImmutable(Bytes.toBytes("i"), Bytes.toBytes("second_smap"), second_level_sitemap);
+            put.addImmutable(Bytes.toBytes("i"), Bytes.toBytes("first_parse_time"), parse_time);
+            put.addImmutable(Bytes.toBytes("i"), Bytes.toBytes("select_ts"), Bytes.toBytes("0")); // default does not select
+            put.addImmutable(Bytes.toBytes("i"), Bytes.toBytes("fetch_ts"), Bytes.toBytes("0")); // default does not fetch
+
+            context.write(new ImmutableBytesWritable(k), put);
+        }
+        catch(Exception e) {
+            context.getCounter("user_mapper", "EXCEPTION").increment(1L);
+        }
+
+    }
+
+}
